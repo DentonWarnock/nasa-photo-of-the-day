04ec5cf12aebd989c90962802ba138ab1cceb511
DentonWarnock <dentonwarnock@gmail.com>
2019-09-24 16:01:36 -0700

    useState, useEffect, and async function added to get data from API key.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React, { useState, useEfftect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import PhotoCard from "./components/PhotoCard.js";  
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        //const photoData = await axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+        const photoData = await axios.get("https://api.nasa.gov/planetary/apod?api_key=DaHDhBGUKq3Eenp4GTC9kKka3MCfNCEDiu55Nm66");
+        console.log(photoData);
+        setData(photoData.data);        
+      } catch (error) {
+        console.log(error);
+      } // try block
+    } // fetchData()
+    fetchData()
+  }, []);
+
   return (
     <div className="App">
       <p>
