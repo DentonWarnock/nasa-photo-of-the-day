87cfe35ff24bb0cf95ef39b3dfb241972216a155
DentonWarnock <dentonwarnock@gmail.com>
2019-09-24 21:01:00 -0700

    Date added to page and button to view previous day's photo. Button is working and changes date, image, and all data when clicked.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,44 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import PhotoCard from "./components/PhotoCard.js";  
+import moment from "moment";
 
 function App() {
   const [data, setData] = useState("test data");
+  const [date, setDate] = useState(moment().format('YYYY-MM-DD'));
+  const [counter, setCounter] = useState(1);
+
+  function handleDateChange () {
+    setDate(moment().subtract(counter, "days").format('YYYY-MM-DD'));
+    setCounter(counter + 1);
+  }
+
+  console.log(date);
 
   useEffect(() => {
     async function fetchData() {
       try {
         //const photoData = await axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
-        const photoData = await axios.get("https://api.nasa.gov/planetary/apod?api_key=DaHDhBGUKq3Eenp4GTC9kKka3MCfNCEDiu55Nm66");
+        const photoData = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=DaHDhBGUKq3Eenp4GTC9kKka3MCfNCEDiu55Nm66&date=${date}`);
         setData(photoData.data);  
         //console.log(data); 
       } catch (error) {
         console.log(error);
       } // try block
     } // fetchData()
+
     fetchData()
-  }, []);
-  console.log(data);
+  }, [date]);
+  
   return (
     <div className="App">
-      <PhotoCard title={data.title} url={data.url} explanation={data.explanation} date={data.date}/>
+      <PhotoCard 
+        title={data.title} 
+        url={data.url} 
+        explanation={data.explanation} 
+        date={date} 
+        handleDateChange={handleDateChange}
+        />
     </div>
   );
 }
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 export default function PhotoCard(props) {
-  return (
+    return (
     <div className="photo-card">
+      <h5>Photo published by NASA on {props.date}</h5>
+      <button onClick={() => props.handleDateChange()}>See previous day's photo</button><br></br><br></br>
       <img src={props.url} alt={props.title}></img>
       <h1>{props.title}</h1>
-      <p>{props.explanation}</p>
-      <h5>Photo published by NASA on {props.date}</h5>
+      <p>{props.explanation}</p>      
     </div>
   )
 }
